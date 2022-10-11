fea841b761598d56a44a42b250a1ce5dda04963e
collegesumeet <thesumeetpay@gmail.com>
2022-10-11 09:51:47 +0530

    yjhh

--- www/js/index.js
+++ www/js/index.js
@@ -27,3 +27,9 @@ function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
 }
+document.addEventListener("deviceready",onDeviceReady);
+
+function onDeviceReady()
+{
+    alert("device is ready by sumeet");
+}
